5a307874e3a2153aa0853453c1e28fd4f6f249f6
Dmitriy Zakharenko <zakharenko@gmail.com>
2020-01-31 15:07:07 +0700

    fix: isPrime func

--- src/games/prime.js
+++ src/games/prime.js
@@ -9,7 +9,7 @@ const wrongAnswer = 'no';
 const isPrime = (number) => {
   if (number < 2) return false;
 
-  for (let i = 2; i < number / 2; i += 1) {
+  for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) return false;
   }
 
